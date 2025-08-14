14526b24b49379da68b4cfd57e9e3f4d68a9acef
Sebastian Kurfuerst <sebastian.kurfuerst@sandstorm.de>
2025-08-14 11:17:35 +0200

    FEATURE: definitions for partitioned waitlists
    (no implementation yet)

--- definition/loader_test.go
+++ definition/loader_test.go
@@ -23,3 +23,28 @@ func TestLoadRecursively_WithMissingDependency(t *testing.T) {
 	_, err := LoadRecursively("../test/fixtures/missingDep.yml")
 	require.EqualError(t, err, `loading ../test/fixtures/missingDep.yml: invalid pipeline definition "test_it": missing task "not_existing" referenced in depends_on of task "test"`)
 }
+
+func TestPartitionedWaitlist_OK(t *testing.T) {
+	_, err := LoadRecursively("../test/fixtures/partitioned_waitlist.yml")
+	require.NoError(t, err)
+}
+
+func TestPartitionedWaitlist_err_0_partition_limit(t *testing.T) {
+	_, err := LoadRecursively("../test/fixtures/partitioned_waitlist_err_0_partition_limit.yml")
+	require.EqualError(t, err, `loading ../test/fixtures/partitioned_waitlist_err_0_partition_limit.yml: invalid pipeline definition "test_it": queue_partition_limit must be defined and >=1 if queue_strategy=partitioned_replace`)
+}
+
+func TestPartitionedWaitlist_err_no_partition_limit(t *testing.T) {
+	_, err := LoadRecursively("../test/fixtures/partitioned_waitlist_err_no_partition_limit.yml")
+	require.EqualError(t, err, `loading ../test/fixtures/partitioned_waitlist_err_no_partition_limit.yml: invalid pipeline definition "test_it": queue_partition_limit must be defined and >=1 if queue_strategy=partitioned_replace`)
+}
+
+func TestPartitionedWaitlist_err_queue_limit(t *testing.T) {
+	_, err := LoadRecursively("../test/fixtures/partitioned_waitlist_err_queue_limit.yml")
+	require.EqualError(t, err, `loading ../test/fixtures/partitioned_waitlist_err_queue_limit.yml: invalid pipeline definition "test_it": queue_limit is not allowed if queue_strategy=partitioned_replace, use queue_partition_limit instead`)
+}
+
+func TestWaitlist_err_partitioned_queue_limit(t *testing.T) {
+	_, err := LoadRecursively("../test/fixtures/waitlist_err_partitioned_queue_limit.yml")
+	require.EqualError(t, err, `loading ../test/fixtures/waitlist_err_partitioned_queue_limit.yml: invalid pipeline definition "test_it": queue_partition_limit is not allowed if queue_strategy=append|replace, use queue_limit instead`)
+}
